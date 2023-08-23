5ace0f5d846bed24d89eea3f41a0a08888a8d76c
RioNanda48 <140035941+RioNanda48@users.noreply.github.com>
2023-08-23 17:30:00 +0700

    Add files via upload

---
+++ latihan_msib5/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introducing Myself</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="back">
+        <div class="box_name">
+            <center>
+                <h1 class="tag">Perkenalkan <br> Nama Saya</h1>
+                <p class="nama"><?php echo "Rio Nanda Prasetyo";?></p>
+            </center>
+        </div>
+        <section class="container">
+      <div class="card">
+        <h1 class="tag">Mahasiswa</h1>
+        <div class="content">
+        <p class="uni"><?php echo "Universitas AKI Semarang";?></p>
+        </div>
+      </div>
+      <div class="card">
+        <h1 class="tag">Jurusan</h1>
+        <div class="content">
+        <p class="jurusan"><?php echo "Teknik Informatika";?></p>
+        </div>
+      </div>
+      <div class="card">
+      <h1 class="tag">Hobi Saya</h1>
+        <div class="content">
+        <p class="hobi"><?php echo "Bermain Game";?><br>
+        <?php echo "Badminton"; ?><br>
+        <?php echo "Menonton Anime"; ?><br>
+        <?php echo "Begadang"; ?></p>
+        </div>
+      </div>
+      <div class="card">
+      <h1 class="tag">Studi Independen</h1>
+        <div class="content">
+        <p class="msib"><?php echo "Saya memilih Mitra Gits karena saya tertarik mempelajari materi full stack developer yang diberikan oleh mitra Gits";?><br>
+        <?php echo "Modul pembelajaran yang diberikan menarik perhatian saya untuk lebih mempelajari bidang full stack web dev"; ?><br>
+        <?php echo "Meskipun saya masih terbilang awam dalam bidang ini"; ?></p>
+        </div>
+      </div>
+    </section>
+    </div>
+</body>
+</html>
